201a53d9ef0434d407577e031ce035a04ec98ddb
codyrancher <cody@rancher.com>
2023-06-19 17:55:47 +0200

    more

--- pkg/opni/product.ts
+++ pkg/opni/product.ts
@@ -20,6 +20,8 @@ export function init(plugin: any, store: any) {
       return;
     }
 
+    console.log('baaaaase', process.env.routerBase);
+
     const clusterOverrideSearchParam = 'c=';
     const clusterId = window.location.search.includes(clusterOverrideSearchParam) ? window.location.search.replace(`?${ clusterOverrideSearchParam }`, '') : 'local';
     const isLocalCluster = clusterId === 'local';
